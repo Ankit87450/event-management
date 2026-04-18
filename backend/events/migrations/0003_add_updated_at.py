f7da9ff594a3ca38251463b99db2bd40d23f23f7
Ankit Modi <ankitmodi87450@gmail.com>
2026-04-19 03:30:31 +0530

    fix bugss of migration

--- backend/events/migrations/0003_add_updated_at.py
+++ backend/events/migrations/0003_add_updated_at.py
@@ -7,9 +7,4 @@ class Migration(migrations.Migration):
         ('events', '0002_add_meal_parking_taken'),
     ]
 
-    operations = [
-        migrations.RunSQL(
-            sql="UPDATE event_management SET updated_at = created_at WHERE updated_at IS NULL;",
-            reverse_sql=migrations.RunSQL.noop,
-        ),
-    ]
+    operations = []
