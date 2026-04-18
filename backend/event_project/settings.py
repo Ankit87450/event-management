eeb287c44c2005b842ae8b9f0a6dfd6415a874b7
Ankit Modi <ankitmodi87450@gmail.com>
2026-04-19 00:32:08 +0530

    Connect Database

--- backend/event_project/settings.py
+++ backend/event_project/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,10 +49,10 @@ TEMPLATES = [{
 WSGI_APPLICATION = 'event_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+    )
 }
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
